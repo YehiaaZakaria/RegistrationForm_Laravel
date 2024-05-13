0f1e8a62b3f25f4ae564c23c6222c3e7d52c708d
YehiaaZakaria <101659043+YehiaaZakaria@users.noreply.github.com>
2024-05-13 13:03:08 +0300

    Moved Helper Functions  to the Main Controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,9 +9,6 @@ use Illuminate\Support\Facades\Mail;
 
 
 
-require_once (app_path() . '/API_Ops.php');
-require_once (app_path() . '/upload.php');
-
 class MainController extends Controller
 {
 
@@ -42,7 +39,7 @@ class MainController extends Controller
                 return;
             }
 
-            $uploadResponse = uploadImage();
+            $uploadResponse = $this->uploadImage();
             $user_image = $_FILES["user_image"]["name"];
 
             if ($uploadResponse == "Ok") {
@@ -74,7 +71,7 @@ class MainController extends Controller
     {
         $day = $request->input('day');
         $month = $request->input('month');
-        $names = getBornTodayNames($day, $month);
+        $names = $this->getBornTodayNames($day, $month);
 
         $html = '<table>';
         $html .= '<tr><td><h3>People born on the same day: <h3></td></tr>';
@@ -85,6 +82,100 @@ class MainController extends Controller
 
         return $html;
     }
+
+    
+    // Helper functions
+
+    function getBornTodayNames($day, $month)
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "https://online-movie-database.p.rapidapi.com/actors/list-born-today?month=$month&day=$day",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "X-RapidAPI-Host: online-movie-database.p.rapidapi.com",
+            "X-RapidAPI-Key: 719c60ac3cmshd22f2140a7ad840p17befajsn63022bc4aaf6"
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+        return [];
+    } else {
+        $pattern = '/\/name\/(nm\d+)\/"/';
+        preg_match_all($pattern, $response, $IDs);
+        $IDs = $IDs[1];
+        $names = [];
+
+        foreach ($IDs as $ID) {
+            $name = $this->getUserName($ID);
+            if ($name !== null) {
+                $names[] = $name;
+            }
+        }
+
+        return $names;
+    }
+}
+
+function getUserName($id)
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "https://online-movie-database.p.rapidapi.com/actors/get-bio?nconst=$id",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "X-RapidAPI-Host: online-movie-database.p.rapidapi.com",
+            "X-RapidAPI-Key: 719c60ac3cmshd22f2140a7ad840p17befajsn63022bc4aaf6"
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        return null; // Return null on error
+    } else {
+        $data = json_decode($response, true);
+        return isset($data['name']) ? $data['name'] : null;
+    }
+}
+
+function uploadImage()
+{
+    $targetDirectory = "uploads/";
+    $targetFile = $targetDirectory . basename($_FILES["user_image"]["name"]);
+
+    $uploadresponse = "";
+
+
+    if (move_uploaded_file($_FILES["user_image"]["tmp_name"], $targetFile)) {
+        $uploadresponse = "Ok";
+    } else {
+        $uploadresponse = "Not Ok";
+    }
+
+    return $uploadresponse;
+}
 
 
 }
